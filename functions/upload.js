0ee1b1adcba9e78abf6eed8b4c219fb1bcd6a27e
taosxyz <93423023+taosxyz@users.noreply.github.com>
2023-05-12 12:58:23 +0800

    Update upload.js

--- functions/upload.js
+++ functions/upload.js
@@ -8,25 +8,19 @@ export async function onRequestPost(context) {  // Contents of context object
      data, // arbitrary space for passing data between middlewares 
      } = context;
      context.request
-	 
      const url = new URL(request.url);
      const response = await fetch('https://telegra.ph/' + url.pathname + url.search, {
          method: request.method,
          headers: request.headers,
          body: request.body,
      });
-     
-	 
-     console.log('https://telegra.ph/' + url.pathname + url.search);
-	console.log(request.headers);
-	console.log(request.body);
+    
+	console.log("context",context);
 	
+	console.log("response",response);
+	const bing_data = await response.json();
 	
-	const bin_data = await response.json();
-	console.log("bin_data", bin_data);
-
-	const info = JSON.stringify(bin_data);
-	
+	console.log("bing_data",bing_data);
 	
 	if(context.request.method === 'OPTIONS'){
 			 return new Response(info, {
@@ -40,24 +34,18 @@ export async function onRequestPost(context) {  // Contents of context object
 	}
 	}
 	
+	new Response(reason, {
+	    status: 200,
+	    statusText: 'Unauthorized',
+	    headers: {
+	      'Content-Type': 'text/plain;charset=UTF-8',
+	      'Cache-Control': 'no-store',
+	      // Returns the "Content-Length" header for HTTP HEAD requests.
+	      'Content-Length': reason.length,
+	    },
+	  });
 	
 	
-	
-	
-	
-	
-	return new Response(info, {
-		status: 200,
-		statusText: '',
-		headers:{
-		'Access-Control-Allow-Origin': request.url,
-		'Access-Control-Allow-Methods': 'POST,GET,OPTIONS',
-		'Access-Control-Allow-Headers': 'x-requested-with, accept, origin, content-type'
-		}
-		
-		});
-     
     return response;
-    
   }
   
